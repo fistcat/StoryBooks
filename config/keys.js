f32c537db840f664366c769311492cdadbef718f
Fistcat <chenxing1991@yahoo.com>
2018-09-07 12:04:44 -0400

    adding google auth

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb://fistcat:zhang.1991@ds157599.mlab.com:57599/storybooks-dev',
+  googleClientID:
+    '1051241452781-rvbpepj5mlhf84lvl5nslq323pem65qr.apps.googleusercontent.com',
+  googleClientSecret: '53ehw40v9F_HAckGumsfftgz'
+};
